9f816e422de728a8d70549bb415aa4ab811be554
mohit99093 <43116791+mohit99093@users.noreply.github.com>
2020-09-23 12:21:52 +0530

    new

---
+++ client/build/precache-manifest.dcf18261a79158c9405f03b503c62762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f0963843a8701b526b347ba1ed9379",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46cde3df3ec314e70d70",
+    "url": "/static/css/2.758d4bd7.chunk.css"
+  },
+  {
+    "revision": "46cde3df3ec314e70d70",
+    "url": "/static/js/2.209f2c72.chunk.js"
+  },
+  {
+    "revision": "61876c10feb06c29abcb1f25684702ec",
+    "url": "/static/js/2.209f2c72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c43ca9bbaf6636e66914",
+    "url": "/static/js/main.b3eb73d5.chunk.js"
+  },
+  {
+    "revision": "d6008d0b122cafe16986",
+    "url": "/static/js/runtime-main.f480b043.js"
+  }
+]);
